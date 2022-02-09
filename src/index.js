31af7895099a1b398b1d983fc36050d219d13286
belveter <dmitry.nayd@gmail.com>
2022-02-09 20:55:18 +0100

    feat: the task is done2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 
 module.exports = function towelSort (matrix) {
     let rez = [];
-
+    if (matrix.length===0) return [];
+    
     matrix.forEach((row, index) => {
         if (index % 2 === 0) {
             for (let i = 0; i < row.length; i++) {
